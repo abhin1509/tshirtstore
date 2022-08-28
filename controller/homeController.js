be8ebf4338a6cd803a4a3f3390fbe29c6a45a80f
Abhinav Kumar <abhin1509@gmail.com>
2022-08-28 13:31:33 +0530

    the big promise

--- controller/homeController.js
+++ controller/homeController.js
@@ -1,13 +1,21 @@
-exports.home = (req, res) => {
+const BigPromise = require("../middleware/bigPromise");
+
+exports.home = BigPromise(async (req, res) => {
   res.status(200).json({
     success: true,
     greeting: "Hello from server",
   });
-};
+});
 
-exports.homeDummy = (req, res) => {
-  res.status(200).json({
-    success: true,
-    greeting: "This is another dummy route",
-  });
+// use either async - await with BigPromise || async - await with try catch
+
+exports.homeDummy = async (req, res) => {
+  try {
+    res.status(200).json({
+      success: true,
+      greeting: "This is another dummy route",
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
