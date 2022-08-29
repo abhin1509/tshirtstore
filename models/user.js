601dd97bde7102a1f7fc0833217e891a567e457f
Abhinav Kumar <abhin1509@gmail.com>
2022-08-29 18:02:55 +0530

    password encryption in mongoose

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -40,5 +41,13 @@ const userSchema = new mongoose.Schema({
     default: Date.now,
   },
 });
+
+// encrypt password before save(pre)
+userSchema.pre('save', async function(next) {
+  if(!this.isModified('password')) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+})
 
 module.exports = mongoose.model("User", userSchema);
