8cbda48ac7cfdd727ffec82bc97e50c8fabf5ef7
Abhinav Kumar <abhin1509@gmail.com>
2022-08-30 14:40:16 +0530

    db connection

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 const app = require("./app");
+const connectWithDb = require('./config/db');
 
 const PORT = process.env.PORT || 3000;
+
+// connect with db
+connectWithDb();
 
 app.listen(PORT, () => console.log(`Server is running at port ${PORT}...`));
