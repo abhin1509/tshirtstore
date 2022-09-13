0be588ea47272b1a7364caac5a85124a8fd21985
Abhinav Kumar <abhin1509@gmail.com>
2022-09-13 16:23:45 +0530

    get all products with where and pager

--- controller/productController.js
+++ controller/productController.js
@@ -1,6 +1,7 @@
 const Product = require("../models/product");
 const BigPromise = require("../middleware/bigPromise");
 const CustomError = require("../utils/customError");
+const WhereClause = require("../utils/whereClause");
 const cloudinary = require("cloudinary").v2;
 
 exports.addProduct = BigPromise(async (req, res, next) => {
@@ -34,5 +35,29 @@ exports.addProduct = BigPromise(async (req, res, next) => {
   res.status(200).json({
     success: true,
     product,
+  });
+});
+
+exports.getAllProduct = BigPromise(async (req, res, next) => {
+  const totalProductCount = await Product.countDocuments();
+  // const products = await Product.find({}); //direct method
+
+  // Better method
+  const resultPerPage = 6; // 3 products in 2 rows
+  const products = new WhereClause(Product.find(), req.query).search().filter();
+
+  const filteredProductCount = products.length;
+
+  // pagination
+  // products.limit().skip();
+  // Better way
+  products.pager(resultPerPage);
+  products = await products.base;
+
+  res.status(200).json({
+    success: true,
+    products,
+    filteredProductCount,
+    totalProductCount,
   });
 });
